8181eaec80470d4be2b9d0c3f76af8051814caec
Sophilex <2646332466@qq.com>
2025-03-06 14:38:44 +0800

    remove useless files

--- tuning3.sh
+++ tuning3.sh
@@ -1,16 +1,16 @@
-python main.py --dataset=gowalla --backbone=simplex --model=rrdvk --gpu_id=2 --cfg rrd_neg=2
-python main.py --dataset=citeulike --backbone=bpr --model=rrdvk --gpu_id=2 --cfg rrd_neg=3
-python main.py --dataset=citeulike --backbone=bpr --model=rrdvk --gpu_id=2 --cfg rrd_neg=4
-python main.py --dataset=citeulike --backbone=bpr --model=rrdvk --gpu_id=2 --cfg rrd_neg=5
-python main.py --dataset=citeulike --backbone=bpr --model=rrdvk --gpu_id=3 --cfg rrd_neg=10
+# python main.py --dataset=gowalla --backbone=simplex --model=rrdvk --gpu_id=2 --cfg rrd_neg=2
+# python main.py --dataset=citeulike --backbone=bpr --model=rrdvk --gpu_id=2 --cfg rrd_neg=3
+# python main.py --dataset=citeulike --backbone=bpr --model=rrdvk --gpu_id=2 --cfg rrd_neg=4
+# python main.py --dataset=citeulike --backbone=bpr --model=rrdvk --gpu_id=2 --cfg rrd_neg=5
+# python main.py --dataset=citeulike --backbone=bpr --model=rrdvk --gpu_id=3 --cfg rrd_neg=10
 
+python main.py --dataset=citeulike --backbone=lightgcn --model=rrdvk --gpu_id=1 --cfg rrd_neg=0
+python main.py --dataset=citeulike --backbone=lightgcn --model=rrdvk --gpu_id=1 --cfg rrd_neg=1
+python main.py --dataset=citeulike --backbone=lightgcn --model=rrdvk --gpu_id=1 --cfg rrd_neg=20
+python main.py --dataset=citeulike --backbone=lightgcn --model=rrdvk --gpu_id=1 --cfg rrd_neg=30
+python main.py --dataset=citeulike --backbone=lightgcn --model=rrdvk --gpu_id=1 --cfg rrd_neg=50
+python main.py --dataset=citeulike --backbone=lightgcn --model=rrdvk --gpu_id=1 --cfg rrd_neg=100
 
-python main.py --dataset=citeulike --backbone=lightgcn --model=rrdvk --gpu_id=2 --cfg rrd_neg=2
-python main.py --dataset=citeulike --backbone=lightgcn --model=rrdvk --gpu_id=2 --cfg rrd_neg=3
-python main.py --dataset=citeulike --backbone=lightgcn --model=rrdvk --gpu_id=2 --cfg rrd_neg=4
-python main.py --dataset=citeulike --backbone=lightgcn --model=rrdvk --gpu_id=2 --cfg rrd_neg=5
-python main.py --dataset=citeulike --backbone=lightgcn --model=rrdvk --gpu_id=3 --cfg rrd_neg=10
-
-python main.py --dataset=gowalla --backbone=simplex --model=rrdvk --cfg rrd_neg=3
-python main.py --dataset=gowalla --backbone=simplex --model=rrdvk --cfg rrd_neg=5
-python main.py --dataset=gowalla --backbone=simplex --model=rrdvk --cfg rrd_neg=10
+# python main.py --dataset=gowalla --backbone=simplex --model=rrdvk --cfg rrd_neg=3
+# python main.py --dataset=gowalla --backbone=simplex --model=rrdvk --cfg rrd_neg=5
+# python main.py --dataset=gowalla --backbone=simplex --model=rrdvk --cfg rrd_neg=10
